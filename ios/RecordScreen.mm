4d664073383cac9292556f83e38db842a8250fd3
lion <lion@esh.com>
2025-01-01 22:27:46 +0200

    remove old RN code

--- ios/RecordScreen.mm
+++ ios/RecordScreen.mm
@@ -290,12 +290,12 @@ RCT_REMAP_METHOD(clean,
 }
 
 // Don't compile this code when we build for the old architecture.
-#ifdef RCT_NEW_ARCH_ENABLED
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeRecordScreenSpecJSI>(params);
-}
-#endif
+// #ifdef RCT_NEW_ARCH_ENABLED
+// - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+//     (const facebook::react::ObjCTurboModule::InitParams &)params
+// {
+//     return std::make_shared<facebook::react::NativeRecordScreenSpecJSI>(params);
+// }
+// #endif
 
 @end
